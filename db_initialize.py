343ff08f3245eb0ba231a0a8a92a59fc03c06b86
Doug Mahugh <dmahugh@google.com>
2019-12-05 09:14:56 -0800

    refactoring, eliminate global variables, define job types

--- db_initialize.py
+++ db_initialize.py
@@ -5,143 +5,81 @@ with this command:
 cloud_sql_proxy.exe -instances=project:region:instance=tcp:1433 -credential_file=credentials.json
 
 """
-from contextlib import suppress
+import csv
 import os
-import shutil
 
 import pyodbc
 
 from config import DB_USER, DB_PASS, DB_NAME
 
-CNXN = pyodbc.connect(
-    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=127.0.0.1;"
-    + f"UID={DB_USER};PWD={DB_PASS}",
-    autocommit=True,
-)
-CURSOR = CNXN.cursor()
+from utils import create_database, print_database, print_table
 
 
-def create_database(db_name):
-    """Create database.
-    """
-    with suppress(pyodbc.ProgrammingError):
-        CURSOR.execute(f"CREATE DATABASE {db_name}")
-        CNXN.commit()
-
-def create_jobdef(db_name):
+def create_jobdef(*, cursor=None, database=None):
     """Create and populate the jobdef table of job definitions.
-    """
-    CURSOR.execute(f"USE {db_name}")
-    CURSOR.execute("DROP TABLE IF EXISTS jobdef")
 
-    sql_command = """
+    jobdef columns:
+    job_id = key field, identifer for this job type
+    start_page = URL of the page from which crawling begins
+    single_domain = whether to only crawl links within startpage's domain (1=True)
+    subpath = optional; if specified, crawling is limited to this subpath of the domain
+    max_pages = maximum number of pages to crawl
+    daily = whether to run this job type every day (1=True)
+    """
+    if not cursor:
+        raise ValueError("print_jobdef: missing required argument (cursor)")
+    if not database:
+        raise ValueError("print_jobdef: missing required argument (database)")
+
+    cursor.execute(f"USE {database}")
+    cursor.execute("DROP TABLE IF EXISTS jobdef")
+    cursor.execute(
+        """
     CREATE TABLE jobdef (
     job_id CHAR(20) PRIMARY KEY,
     start_page VARCHAR(120),
     single_domain BIT,
     subpath VARCHAR(120),
     max_pages INT,
-    active BIT);"""
-    CURSOR.execute(sql_command)
+    daily BIT);"""
+    )
 
-    job_id = "Cloud SQL docs"
-    start_page = "https://cloud.google.com/sql/docs/"
-    single_domain = 1
-    subpath = "/sql/docs"
-    max_pages = 10
-    active = 1
-    sql_command = f"INSERT INTO jobdef (job_id, start_page, single_domain, subpath, max_pages, active) VALUES ('{job_id}', '{start_page}', '{single_domain}', '{subpath}', '{max_pages}', '{active}')"
-    CURSOR.execute(sql_command)
+    with open("jobdef.csv", "r") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",", quotechar='"')
+        next(csvreader)  # skip header row
+        for row in csvreader:
+            job_id, start_page, single_domain, subpath, max_pages, daily = row
+            sql_command = f"INSERT INTO jobdef (job_id, start_page, single_domain, subpath, max_pages, daily) VALUES ('{job_id}', '{start_page}', '{single_domain}', '{subpath}', '{max_pages}', '{daily}')"
+            cursor.execute(sql_command)
 
-    CNXN.commit()
-
-
-def column_names(cursor, table_name):
-    """Returns a list of the column names for a table.
-    """
-    cursor.execute(f"USE {DB_NAME}")
-    cursor.execute(f"exec sp_columns '{table_name}'")
-    results = cursor.fetchall()
-    return [result[1] for result in results]
+    cursor.commit()
 
 
-def print_jobdef(db_name):
+def print_jobdef(*, cursor=None, database=None):
     """Print contents of the jobdef table.
     """
+    if not cursor:
+        raise ValueError("print_jobdef: missing required argument (cursor)")
+    if not database:
+        raise ValueError("print_jobdef: missing required argument (database)")
 
-    CURSOR.execute("SELECT job_id, start_page FROM jobdef;")
-    print_table(cursor=CURSOR, title="jobdef table", rows=99)
-
-
-def print_database(db_name):
-    """Print a summary of the tables in a database file.
-    """
-    print(f"{db_name}")
-    CURSOR.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = [_[0] for _ in CURSOR.fetchall() if not _[0].startswith("sqlite_")]
-    for table in tables:
-        print(f"+- {table} ({','.join(column_names(CURSOR, table))})")
-
-
-def print_table(*, db_name=None, cursor=None, table=None, title=None, rows=10):
-    """Print contents of a table or cursor.
-    This is for quick printing of small data sets for diagnostic purposes, may
-    not work well with large numbers of columns.
-    """
-    if not cursor and not (db_name and table):
-        raise ValueError("print_table requires cursor or db_name/table")
-
-    if not title:
-        title = "cursor" if cursor else f"{table} table"
-
-    if cursor:
-        columns = [row[0] for row in cursor.description]
-    else:
-        cursor = CURSOR
-        columns = column_names(cursor, table)
-
-    # default column width is the length of each column name
-    widths = [len(column) for column in columns]
-
-    # adjust column widths based on the data found in each column
-    if db_name and table:
-        cursor.execute(f"SELECT * FROM {table}")
-    cursor_rows = cursor.fetchall()
-    for ncol, col_width in enumerate(widths):
-        max_data_width = max([len(str(row[ncol])) for row in cursor_rows[:rows]])
-        widths[ncol] = max(col_width, max_data_width)
-
-    # all output lines will be truncated to console_width characters
-    console_width = shutil.get_terminal_size((80, 20))[0] - 1
-
-    # print header rows
-    tot_width = min(sum(widths) + len(columns) - 1, console_width)
-    title_plus_rowcount = f"{title} ({len(cursor_rows)} rows)"
-    print("\n" + f" {title_plus_rowcount} ".center(tot_width, "-"))
-    column_headers = " ".join(
-        [column.ljust(width) for column, width in zip(columns, widths)]
-    )
-    print(column_headers[:console_width])
-    underlines = " ".join([width * "-" for width in widths])
-    print(underlines[:console_width])
-
-    # print data rows
-    for row_number, row in enumerate(cursor_rows):
-        printed_row = " ".join(
-            [str(value).ljust(width) for value, width in zip(row, widths)]
-        )
-        print(printed_row[:console_width])
-        if row_number >= rows - 1:
-            break
+    cursor.execute("SELECT * FROM jobdef ORDER BY daily, job_id;")
+    print_table(cursor=cursor, title="jobdef table", rows=99)
 
 
 def main():
-    """Create an acme_corp database and populate some tables with random data
+    """Main function.
     """
-    create_database(DB_NAME)
-    create_jobdef(DB_NAME)
-    print_jobdef(DB_NAME)
+    cursor = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER=127.0.0.1;"
+        + f"UID={DB_USER};PWD={DB_PASS}",
+        autocommit=True,
+    ).cursor()
 
+    create_database(cursor=cursor, database=DB_NAME)
+    create_jobdef(cursor=cursor, database=DB_NAME)
+    print_jobdef(cursor=cursor, database=DB_NAME)
+    print_database(cursor=cursor, database=DB_NAME)
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,120 @@
+"""Utility functions for Cloud SQL and related services.
+"""
+from contextlib import suppress
+import shutil
+
+import pyodbc
+
+
+def create_database(*, cursor=None, database=None, drop=False):
+    """Create database.
+    """
+    if not cursor:
+        raise ValueError("create_database: missing required argument (cursor)")
+    if not database:
+        raise ValueError("create_database: missing required argument (database)")
+
+    if drop:
+        with suppress(pyodbc.ProgrammingError):
+            cursor.execute(f"DROP DATABASE {database}")
+
+    with suppress(pyodbc.ProgrammingError):
+        cursor.execute(f"CREATE DATABASE {database}")
+        cursor.commit()
+
+
+def column_names(*, cursor=None, table=None, database=None):
+    """Returns a list of the column names for a table.
+    """
+    if not cursor:
+        raise ValueError("column_names: missing required argument (cursor)")
+    if not table:
+        raise ValueError("column_names: missing required argument (table)")
+
+    if database:
+        cursor.execute(f"USE {database}")
+
+    cursor.execute(f"select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='{table}'")
+    results = cursor.fetchall()
+    return [result[3] for result in results]
+
+
+def print_database(*, cursor=None, database=None):
+    """Print a summary of the tables in a database.
+    """
+    if not cursor:
+        raise ValueError("print_database: missing required argument (cursor)")
+    if not database:
+        raise ValueError("print_database: missing required argument (database)")
+
+    print(f"\ndatabase: {database}")
+    cursor.execute(f"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='{database}';")
+    tables = [_[0] for _ in cursor.fetchall()]
+    for table in tables:
+        print(f"{table} table: {', '.join(column_names(cursor=cursor, table=table))}")
+
+
+def print_table(*, table=None, cursor=None, database=None, title=None, rows=10):
+    """Print contents of a table or cursor to the console. This is for quick
+    printing of small result sets for diagnostic purposes, may not work well
+    with a large numbers of columns.
+
+    Args:
+        table: name of table to be printed; if not provided, the result set
+               in the cursor will be printed.
+        cursor: a cursor from a pyodbc connection; if table is provided, a
+                SELECT * will be executed in this cursor; if table is not
+                provided, the cursor should contain a result set (i.e., the
+                most recent SQL query was a SELECT.
+        database: database name for the table (optional)
+        title: optional title
+        rows: maximum number of rows to print
+    """
+    if not cursor:
+        raise ValueError("print_table: missing required argument (cursor)")
+
+    if not title:
+        title = "cursor" if cursor else f"{table} table"
+
+    if table:
+        columns = column_names(cursor=cursor, table=table)
+    else:
+        columns = [row[0] for row in cursor.description]
+
+    # default column width is the length of each column name
+    widths = [len(column) for column in columns]
+
+    # adjust column widths based on the data found in each column
+    if table:
+        if database:
+            cursor.execute(f"USE {database}")
+        cursor.execute(f"SELECT * FROM {table}")
+    cursor_rows = cursor.fetchall()
+    for ncol, col_width in enumerate(widths):
+        max_data_width = max([len(str(row[ncol])) for row in cursor_rows[:rows]])
+        widths[ncol] = max(col_width, max_data_width)
+
+    # all output lines will be truncated to console_width characters
+    console_width = shutil.get_terminal_size((80, 20))[0] - 1
+
+    # print header rows
+    tot_width = min(sum(widths) + len(columns) - 1, console_width)
+    title_plus_rowcount = f"{title} ({len(cursor_rows)} rows)"
+    print("\n" + f" {title_plus_rowcount} ".center(tot_width, "-"))
+    column_headers = " ".join(
+        [column.ljust(width) for column, width in zip(columns, widths)]
+    )
+    print(column_headers[:console_width])
+    underlines = " ".join([width * "-" for width in widths])
+    print(underlines[:console_width])
+
+    # print data rows
+    for row_number, row in enumerate(cursor_rows):
+        printed_row = " ".join(
+            [str(value).ljust(width) for value, width in zip(row, widths)]
+        )
+        print(printed_row[:console_width])
+        if row_number >= rows - 1:
+            break
+
+
